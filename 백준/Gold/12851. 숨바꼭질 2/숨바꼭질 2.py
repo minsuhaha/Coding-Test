fd7af804ed35762236aed03a1ca20c29b38622d0
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-07-05 13:38:23 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 128 ms, Memory: 35300 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,33 +1,32 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
+INF = float('inf')
 
 def bfs(x):
     queue = deque([x])
-    second = 100001
+    second = INF
     case = 0
 
     while queue:
         x = queue.popleft()
-        
-        if dist[x] > second:
+
+        if point[x] > second:
             break
 
         if x == K:
-            second = dist[x]
-            case += 1    
+            second = point[x] # 가장 빨리 발견되는 초 second 변수에 담기
+            case += 1
+            continue
 
         for nx in (x-1, x+1, 2*x):
-            if 0 <= nx <= 100000 and (dist[nx] == dist[x] + 1 or dist[nx] == 0) :
+            if 0 <= nx <= 100000 and (point[nx] == point[x] + 1 or point[nx] == 0): # 동일한 초에 속하는 값들은 queue에 넣어줘도 됨
                 queue.append(nx)
-                dist[nx] = dist[x] + 1
-
+                point[nx] = point[x] + 1
     return second, case
 
 N, K = map(int, input().split())
-dist = [0] * 100001
+point = [0] * 100001
 second, case = bfs(N)
 print(second)
 print(case)
-
