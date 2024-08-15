d0818577d69ad8f195b420331eff052d22f7dbbf
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-08-15 20:35:19 +0900

    [Gold III] Title: 불!, Time: 1156 ms, Memory: 39480 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,58 +1,53 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
+R, C = map(int, input().split())
+graph = [list(input().rstrip()) for _ in range(R)]
 
-r, c = map(int, input().split())
-graph = [list(input().rstrip()) for _ in range(r)]
-
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-
-fqueue = deque() # 초기 불이 있는 위치
-jqueue = deque() # 초기 지훈이가 있는 위치
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def bfs(fqueue, jqueue):
-    minute = 0 # 시간 초기화(분)
-
+    minute = 0
     while jqueue:
-        minute += 1 # 1분씩 올라감
-
+        minute += 1
         while fqueue and fqueue[0][2] < minute:
-            y, x, time = fqueue.popleft()
+            x, y, m = fqueue.popleft()
 
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if nx < 0 or nx > c-1 or ny < 0 or ny > r-1:
+                if nx < 0 or nx > R-1 or ny < 0 or ny > C-1:
                     continue
-                if graph[ny][nx] == '.':
-                    graph[ny][nx] = 'F' # 불로 번짐표시
-                    fqueue.append((ny, nx, time+1))
+
+                if graph[nx][ny] == '.':
+                    graph[nx][ny] = 'F'
+                    fqueue.append((nx, ny, m+1))
 
         while jqueue and jqueue[0][2] < minute:
-            y, x, time = jqueue.popleft()
+            x, y, m = jqueue.popleft()
 
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if nx < 0 or nx > c-1 or ny < 0 or ny > r-1:
+                if nx < 0 or nx > R-1 or ny < 0 or ny > C-1:
                     return minute
-                
-                if graph[ny][nx] == '.':
-                    jqueue.append((ny, nx, time+1))
-                    graph[ny][nx] = '..' # 지훈이 방문처리
+                if graph[nx][ny] == '.':
+                    graph[nx][ny] = '..'
+                    jqueue.append((nx, ny, m+1))
 
     return 'IMPOSSIBLE'
 
-for i in range(r):
-    for j in range(c):
+fqueue, jqueue = deque(), deque()
+for i in range(R):
+    for j in range(C):
         if graph[i][j] == 'F':
             fqueue.append((i, j, 0))
-        elif graph[i][j] == 'J':
+        if graph[i][j] == 'J':
             jqueue.append((i, j, 0))
-            graph[i][j] = '.' # 지훈이 방분처리        
+            graph[i][j] = '..'
 
-res = bfs(fqueue, jqueue)
-print(res)       
+print(bfs(fqueue, jqueue))
