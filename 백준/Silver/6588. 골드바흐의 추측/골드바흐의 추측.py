b0a7db1ce88d3bfd47ea554de2846f20123fb311
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-08-22 23:03:42 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1516 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 
 prime = [] # 소수를 담을 리스트
 check = [0] * 1000001
-for i in range(2, 1000001): # 주어진 조건이 100000이하의 짝수범위라서
+for i in range(2, int(1000001**0.5)+1): # 주어진 조건이 100000이하의 짝수범위라서
     if check[i] == 0:
         prime.append(i)
-    for i in range(i*i, 1000001, i):
+    for i in range(i**2, 1000001, i):
         check[i] = 1 # 소수 아님
 
 while True:
@@ -17,7 +17,7 @@ while True:
         flag = False
         for p in prime:
             is_prime = n - p
-            if is_prime > 1 and not check[is_prime]: # 소수라면
+            if not check[is_prime]: # 소수라면
                 print(f'{n} = {p} + {is_prime}')
                 flag = True
                 break
