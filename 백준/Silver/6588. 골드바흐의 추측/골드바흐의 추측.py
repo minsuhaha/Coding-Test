1fc718d4ed7fdffaaa919b615b0b77965de0b6be
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-08-22 22:47:22 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1924 ms, Memory: 41600 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+prime = [] # 소수를 담을 리스트
+check = [0] * 1000001
+for i in range(2, 1000001): # 주어진 조건이 100000이하의 짝수범위라서
+    if check[i] == 0:
+        prime.append(i)
+    for i in range(i*i, 1000001, i):
+        check[i] = 1 # 소수 아님
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        flag = False
+        for p in prime:
+            is_prime = n - p
+            if is_prime > 1 and not check[is_prime]: # 소수라면
+                print(f'{n} = {p} + {is_prime}')
+                flag = True
+                break
+        if not flag:
+            print("Goldbach's conjecture is wrong")
