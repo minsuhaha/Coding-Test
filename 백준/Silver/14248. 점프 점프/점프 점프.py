8ead27820b7ec9ca42e3ab9356a770d975e2e45a
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-08-25 22:28:29 +0900

    [Silver II] Title: 점프 점프, Time: 72 ms, Memory: 42184 KB -BaekjoonHub

---
+++ 백준/Silver/14248. 점프 점프/점프 점프.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n = int(input())
+graph = [0] + list(map(int, input().split()))
+visited = [False] * (n+1)
+s = int(input())
+
+def bfs(x):
+    queue = deque([x])
+    visited[x] = True
+    cnt = 1
+
+    while queue:
+        x = queue.popleft()
+        
+        # 왼쪽 이동
+        if 0 < x - graph[x] < n+1:
+            if not visited[x-graph[x]]:
+                cnt += 1
+                queue.append(x-graph[x])
+                visited[x-graph[x]] = True
+        # 오른쪽 이동
+        if 0 < x + graph[x] < n+1:
+            if not visited[x+graph[x]]:
+                cnt += 1
+                queue.append(x+graph[x])
+                visited[x+graph[x]] = True
+    return cnt
+
+print(bfs(s))
