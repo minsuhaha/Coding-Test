49cbe206f897a5e42a5104265c53aab97cd2a67c
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-05 09:25:39 +0900

    [level 2] Title: 프로세스, Time: 0.79 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -3,15 +3,23 @@ def solution(priorities, location):
     queue = deque((idx, num) for idx, num in enumerate(priorities))
     answer = []
     
+    # while queue:
+    #     idx, num = queue.popleft()
+    #     flag = True
+    #     for idx2, num2 in queue:
+    #         if num < num2:
+    #             queue.append((idx, num))
+    #             flag = False
+    #             break
+    #     if flag:
+    #         answer.append(idx)
+    
     while queue:
         idx, num = queue.popleft()
-        flag = True
-        for idx2, num2 in queue:
-            if num < num2:
-                queue.append((idx, num))
-                flag = False
-                break
-        if flag:
+        
+        if any(num < num2 for idx2, num2 in queue):
+            queue.append((idx, num))
+        else:
             answer.append(idx)
             
     return answer.index(location)+1
