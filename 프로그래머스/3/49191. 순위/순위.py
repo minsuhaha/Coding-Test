0701987121a34dae4f85afebcd54f7895635e6ee
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-06 11:44:30 +0900

    [level 3] Title: 순위, Time: 20.41 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,31 @@
+from collections import deque, Counter
+def solution(n, results):
+    graph_win = [[] for _ in range(n+1)]
+    graph_lose = [[] for _ in range(n+1)]
+    
+    for win_player, lose_player in results:
+        graph_win[win_player].append(lose_player)
+        graph_lose[lose_player].append(win_player)
+    
+    def bfs(graph, x):
+        queue = deque([x])
+        visited = [False]*(n+1)
+        cnt = 0
+        
+        while queue:
+            node = queue.popleft()
+            
+            for next_node in graph[node]:
+                if not visited[next_node]:
+                    visited[next_node] = True
+                    queue.append((next_node))
+                    cnt += 1
+        return cnt
+    
+    answer = 0
+    for i in range(1, n+1):
+        if bfs(graph_win, i) + bfs(graph_lose, i) == n-1:
+            answer += 1
+    
+    return answer
+    
