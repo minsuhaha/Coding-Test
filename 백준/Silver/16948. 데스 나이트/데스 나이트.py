afcd9370d5380303abe18433680dc1c2ad70312e
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-25 11:00:08 +0900

    [Silver I] Title: 데스 나이트, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+    visited[x][y] = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<n and 0<=ny<n:
+                if nx == r2 and ny == c2:
+                    return visited[x][y]
+                if not visited[nx][ny]:
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.append((nx, ny))
+    return -1
+
+n = int(input())
+graph = [[0]*n for _ in range(n)]
+r1, c1, r2, c2 = map(int, input().split())
+
+dx = [-2, -2, 0, 0, 2, 2]
+dy = [-1, 1, -2, 2, -1, 1]
+
+visited = [[0]*n for _ in range(n)]
+print(bfs(r1, c1))
