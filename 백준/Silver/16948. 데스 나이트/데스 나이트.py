7d75741a4ba1d9fcfb9189fd001688b6f409cdfa
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-25 11:01:53 +0900

    [Silver I] Title: 데스 나이트, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 
 def bfs(x, y):
     queue = deque([(x, y)])
-    visited[x][y] = 1
+    visited[x][y] = 0
 
     while queue:
         x, y = queue.popleft()
@@ -16,8 +16,8 @@ def bfs(x, y):
 
             if 0<=nx<n and 0<=ny<n:
                 if nx == r2 and ny == c2:
-                    return visited[x][y]
-                if not visited[nx][ny]:
+                    return visited[x][y] + 1
+                if visited[nx][ny] == -1:
                     visited[nx][ny] = visited[x][y] + 1
                     queue.append((nx, ny))
     return -1
@@ -29,5 +29,5 @@ r1, c1, r2, c2 = map(int, input().split())
 dx = [-2, -2, 0, 0, 2, 2]
 dy = [-1, 1, -2, 2, -1, 1]
 
-visited = [[0]*n for _ in range(n)]
+visited = [[-1]*n for _ in range(n)]
 print(bfs(r1, c1))
