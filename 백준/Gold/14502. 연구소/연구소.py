b3c195ecc2ff5533058e0d805f4ac0872ba4c73e
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-26 12:08:20 +0900

    [Gold IV] Title: 연구소, Time: 2316 ms, Memory: 34152 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,60 +1,68 @@
-import sys
-import copy
-from collections import deque
-input = sys.stdin.readline
+'''
+문제
+일부 칸은 바이러스가 존재하며, 이 바이러스는 상하좌우로 인접한 빈 칸으로 모두 퍼져나갈 수 있다. 
+새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다.
 
+0 : 빈 칸 
+1 :벽
+2 : 바이러스
+
+결론
+벽을 3개 세워서 바이러스가 모든 빈 칸으로 퍼지지않도록 막을 경우의 안전영역 칸 개수 구하기
+'''
+import sys, copy
+from collections import deque
+from itertools import combinations
+input = sys.stdin.readline
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 
-def back(cnt):
-    global count
-    if cnt == 3:    
-        count = max(bfs(), count)
-        return
-    
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 0:
-                graph[i][j] = 1 # 벽으로 만들어주기
-                cnt += 1
-                back(cnt)
-                graph[i][j] = 0
-                cnt -= 1
-
-def bfs():
-    queue = deque()
-
+def bfs(comb):
     graph_copy = copy.deepcopy(graph)
-    for i in range(N):
-        for j in range(M):
+    cnt = 0
+
+    # 벽으로 변경
+    for com in comb:
+        graph_copy[com[0]][com[1]] = 1
+
+    # 바이러스 초기 위치 queue에 넣어주기
+    queue = deque()
+    for i in range(n):
+        for j in range(m):
             if graph_copy[i][j] == 2:
                 queue.append((i, j))
-
+                
+    
     while queue:
-        x, y = queue.popleft()
+        y, x = queue.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph_copy[nx][ny] == 0:
-                    graph_copy[nx][ny] = 2 # 바이러스 퍼짐
-                    queue.append((nx, ny))
+            if 0<=nx<m and 0<=ny<n:
+                if graph_copy[ny][nx] == 0:
+                    graph_copy[ny][nx] = 2
+                    queue.append((ny, nx))
     
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
+    for i in range(n):
+        for j in range(m):
             if graph_copy[i][j] == 0:
                 cnt += 1
     
-    if cnt:
-        return cnt
-    return 0
+    return cnt
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-count = 0
-back(0)
-print(count)
+n, m = map(int, input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+blank = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            blank.append((i, j))
+
+max_cnt = 0
+for comb in combinations(blank, 3):
+    max_cnt = max(max_cnt, bfs(comb))
+print(max_cnt)
