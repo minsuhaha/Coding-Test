ed8e299b66cac247ee209b06d8af07a145fda42f
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-26 22:41:00 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3696 ms, Memory: 188132 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,42 +1,48 @@
+'''
+문제
+ 0 : 이동 가능 칸
+ 1 : 벽
+* 도중에 한 개의 벽을 부수고 이동하는 것이 좀 더 경로가 짧아진다면, 벽을 한 개 까지 부수고 이동 가능
+
+결론
+(1, 1) -> (N, M) 이동하는데 최단거리
+'''
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
 
-def bfs(x, y, z):
-    queue = deque([(x, y, z)])
-    visited[x][y][z] = 1
-    
+def bfs(x, y):
+    queue = deque([(x, y, 0)])
+    visited[y][x][0] = 1
+
     while queue:
         x, y, z = queue.popleft()
 
-        if x == N-1 and y == M-1:
-            return visited[x][y][z]
+        if x == m-1 and y == n-1:
+            return visited[y][x][z]
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if nx < 0 or nx > N-1 or ny < 0 or ny > M-1:
-                continue
-            
-            # 이동 가능 한 칸이라면 (벽을 이미 부셨는지 여부 상관없이)
-            if graph[nx][ny] == 0 and visited[nx][ny][z] == 0: # 해당 칸은 이동 할 수 있으면 아직 방문하지 않음
-                visited[nx][ny][z] = visited[x][y][z] + 1
-                queue.append((nx, ny, z))
-            
-            # 벽을 부술 수 있는 상황이라면
-            elif graph[nx][ny] == 1 and z == 0:
-                visited[nx][ny][1] = visited[x][y][z] + 1
-                queue.append((nx, ny, 1))
-    return -1
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().rstrip())) for _ in range(N)]
-visited = [[[0] * 2 for _ in range(M)]for _ in range(N)]
- # Z = 0 : 벽을 아직 안 부수고 이동 중 / Z = 1 : 벽을 이미 한개 부시고 이동 중
+            if 0<=nx<m and 0<=ny<n:
+                if not visited[ny][nx][z]:
+                    if graph[ny][nx] == '0':
+                        visited[ny][nx][z] = visited[y][x][z] + 1
+                        queue.append((nx, ny, z))
 
-res = bfs(0, 0, 0)
-print(res)
+                    elif graph[ny][nx] == '1' and z == 0:
+                        visited[ny][nx][1] = visited[y][x][z] + 1
+                        queue.append((nx, ny, z+1))
+
+    return -1    
+
+n, m = map(int, input().split())
+graph = [list(input().rstrip()) for _ in range(n)]
+visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+print(bfs(0, 0))
