a7268041bec742e98c0184ff6921b71f4291bf86
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-09-27 13:09:49 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 5364 ms, Memory: 473664 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,35 +1,44 @@
+'''
+문제
+0 : 이동가능
+1 : 벽
+* 도중에 벽을 부수고 이동하는 것이 좀 더 경로가 짧아진다면, 벽을 K개 까지 부수고 이동
+
+결론
+(1, 1)에서 (N, M)의 위치까지 이동 -> 최단거리 구하기
+
+'''
 import sys
 from collections import deque
+input = sys.stdin.readline
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 
-def bfs(x, y):
-    queue = deque([(x, y, 0)])
-    visited[x][y][0] = 1
+def bfs(x, y, z):
+    queue = deque([(x, y, z)])
+    visited[y][x][z] = 1
 
     while queue:
-        x, y, k = queue.popleft()
+        x, y, z = queue.popleft()
 
-        if x == N-1 and y == M-1:
-            return visited[x][y][k]
+        if x == M-1 and y == N-1:
+            return visited[y][x][z]
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph[nx][ny] == 0 and not visited[nx][ny][k]:
-                    visited[nx][ny][k] = visited[x][y][k] + 1
-                    queue.append((nx, ny, k))
-                
-                elif graph[nx][ny] == 1 and k < K and not visited[nx][ny][k+1]:
-                    visited[nx][ny][k+1] = visited[x][y][k] + 1
-                    queue.append((nx, ny, k+1))
+            if 0<=nx<M and 0<=ny<N:
+                if graph[ny][nx] == 1 and z < K and not visited[ny][nx][z+1]:
+                    visited[ny][nx][z+1] = visited[y][x][z] + 1
+                    queue.append((nx, ny, z+1))
+                elif graph[ny][nx] == 0 and not visited[ny][nx][z]:
+                    visited[ny][nx][z] = visited[y][x][z] + 1
+                    queue.append((nx, ny, z))
     return -1
 
 N, M, K = map(int, input().split())
-graph = [list(map(int, input()))for _ in range(N)]
+graph = [list(map(int, input().rstrip())) for _ in range(N)]
 visited = [[[0] * (K+1) for _ in range(M)] for _ in range(N)]
-print(bfs(0, 0))
+print(bfs(0, 0, 0))
