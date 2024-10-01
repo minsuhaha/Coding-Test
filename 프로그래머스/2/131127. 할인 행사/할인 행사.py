54572deb1ec4bd1bb314a19bd180fff2ca67d681
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-10-01 18:37:58 +0900

    [level 2] Title: 할인 행사, Time: 175.64 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,25 +1,11 @@
 from collections import Counter
 def solution(want, number, discount):
-    res = 0
-    want_dict = {}
-    for w, n in zip(want, number):
-        want_dict[w] = n
-    
-    discount_cnt = Counter(discount[:10])
-    
-    if discount_cnt == want_dict:
-        res += 1
-
-    for i in range(10, len(discount)):
-        discount_cnt[discount[i]] += 1
-        discount_cnt[discount[i-10]] -= 1
+    want_count = {}
+    for i in range(len(number)):
+        want_count[want[i]] = number[i]
         
-        if discount_cnt[discount[i-10]] == 0:
-            del discount_cnt[discount[i-10]]
-            
-        if discount_cnt == want_dict:
-            res += 1
-        
-    return res
-                
-        
+    cnt = 0
+    for i in range(len(discount)-9):
+        if want_count == Counter(discount[i:i+10]):
+            cnt += 1
+    return cnt
