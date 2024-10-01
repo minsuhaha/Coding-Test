930e5c22b256c5785fb21ac99142167afb5314a2
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-10-01 19:29:56 +0900

    [Gold V] Title: 방탈출, Time: 4632 ms, Memory: 200472 KB -BaekjoonHub

---
+++ 백준/Gold/23352. 방탈출/방탈출.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(sx, sy, cnt):
+    queue = deque([(sx, sy, cnt)])
+    visited = [[False]*m for _ in range(n)]
+    visited[sx][sy] = True
+
+    while queue:
+        x, y, cnt = queue.popleft()
+
+        flag = False
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny] != 0 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny, cnt+1))
+                    flag = True
+        if not flag:
+            ans.append((cnt, graph[sx][sy]+graph[x][y])) 
+
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+ans = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] != 0:
+            bfs(i, j, 0)
+
+ans.sort(key=lambda x : (-x[0], -x[1]))
+print(ans[0][1])
