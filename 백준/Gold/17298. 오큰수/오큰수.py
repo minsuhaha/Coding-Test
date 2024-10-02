8fbc25b1561afd8572b402b8d4326afc68703735
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-10-02 15:40:16 +0900

    [Gold IV] Title: 오큰수, Time: 912 ms, Memory: 155668 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,14 +1,21 @@
+'''
+문제
+-  Ai의 오큰수는 오른쪽에 있으면서 Ai보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오큰수는 -1이다.
+-  A = [3, 5, 2, 7]인 경우 NGE(1) = 5, NGE(2) = 7, NGE(3) = 7, NGE(4) = -1이다. 
+-  A = [9, 5, 4, 8]인 경우에는 NGE(1) = -1, NGE(2) = 8, NGE(3) = 8, NGE(4) = -1
+'''
+
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-numbers = list(map(int, input().split()))
-answer = [-1] * N
+n = int(input())
+num = list(map(int, input().split()))
+ans = [-1] * n
 stack = [0]
 
-for i in range(1, N):
-    while stack and numbers[stack[-1]] < numbers[i]:
-        answer[stack.pop()] = numbers[i]
+for i in range(1, n):
+    while stack and num[stack[-1]] < num[i]:
+        ans[stack.pop()] = num[i]
     stack.append(i)
 
-print(*answer)
+print(*ans)
