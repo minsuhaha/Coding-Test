095d7d8832ed341d45a75989d007dfe20d027d69
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-10-08 13:01:37 +0900

    [Gold III] Title: 다리 만들기, Time: 1644 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -3,12 +3,15 @@ from collections import deque
 input = sys.stdin.readline
 INF = float('inf')
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-def bfs(x, y, num):
-    queue = deque([(x, y)])
-    graph[x][y] = num # 방문표시
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y, cnt):
+    queue = deque([(x, y)])    
+    graph[x][y] = cnt
 
     while queue:
         x, y = queue.popleft()
@@ -17,55 +20,45 @@ def bfs(x, y, num):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx > N-1 or ny < 0 or ny > N-1:
-                continue
-            
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = num
-                queue.append((nx, ny))
+            if 0<=nx<n and 0<=ny<n:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = cnt
+                    queue.append((nx, ny))                    
 
-def check(z):
-    queue = deque([])
-    visited = [[False]*N for _ in range(N)]
-
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] == z:
+def bfs2(num):
+    queue = deque()
+    visited = [[False]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == num:
                 queue.append((i, j, 0))
 
     while queue:
-        x, y, k = queue.popleft()
-        
+        x, y, d = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx > N-1 or ny < 0 or ny > N-1:
-                continue
-            
-            # 다른 육지 일때
-            if graph[nx][ny] > 1 and graph[nx][ny] != z:
-                return k
-            
-            # 바다 일때
-            if graph[nx][ny] == 0 and not visited[nx][ny]:
-                queue.append((nx, ny, k+1))
-                visited[nx][ny] = True
-
+            if 0<=nx<n and 0<=ny<n:
+                if graph[nx][ny] != num and graph[nx][ny] > 1:
+                    return d
+                
+                if graph[nx][ny] == 0 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny, d+1))
     return INF
-
-N = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
-
-num = 2 # 임의의 유지 번호
-for i in range(N):
-    for j in range(N):
+                    
+# 1. 섬끼리 묶기
+cnt = 1
+for i in range(n):
+    for j in range(n):
         if graph[i][j] == 1:
-            bfs(i, j, num)
-            num += 1 
+            cnt += 1
+            bfs(i, j, cnt)
 
+# 2. 섬끼리의 최소거리 구하기
 ans = INF
-for i in range(2, num):
-    ans = min(check(i), ans)
-
+for i in range(2, cnt+1):
+    ans = min(ans, bfs2(i))
 print(ans)
