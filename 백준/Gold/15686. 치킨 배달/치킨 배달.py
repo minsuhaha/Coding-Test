aa7657a23d25b8aff435339d6c53a7e252ec2a0d
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-10-10 16:15:00 +0900

    [Gold V] Title: 치킨 배달, Time: 556 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,28 +1,28 @@
 import sys
+from collections import deque
 from itertools import combinations
 input = sys.stdin.readline
+
 INF = float('inf')
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-# 치킨 집, 일반 집 위치값 넣어주기
 chicken, house = [], []
-for i in range(N):
-    for j in range(N):
+for i in range(n):
+    for j in range(n):
         if graph[i][j] == 2:
-            chicken.append((i, j))
+            chicken.append((i,j))
         elif graph[i][j] == 1:
-            house.append((i, j))
+            house.append((i,j))
 
-res = []
-for comb in combinations(chicken, M):
-    total = 0
+total_chicken_dist = INF
+for comb in combinations(chicken, m):
+    now_chicken_dist = 0
     for h in house:
-        cnt = INF
+        dist = INF
         for c in comb:
-            cnt = min(cnt, abs(h[0]-c[0]) + abs(h[1]-c[1]))
-        total += cnt
-    res.append(total)
-
-print(min(res))
+            dist = min(dist, abs(h[0]-c[0])+abs(h[1]-c[1]))
+        now_chicken_dist += dist
+    total_chicken_dist = min(total_chicken_dist, now_chicken_dist)
+print(total_chicken_dist)
