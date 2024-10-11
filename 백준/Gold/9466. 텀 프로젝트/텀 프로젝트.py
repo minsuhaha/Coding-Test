6d4aa58d0559f68050e1954ef13e121d3fbb1abf
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-10-11 12:53:52 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2508 ms, Memory: 49112 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -3,34 +3,33 @@ from collections import deque
 input = sys.stdin.readline
 
 
-def bfs(num):
-    queue = deque([num])
-    visited[num] = True
-    cycle = []
+def bfs(node):
+    queue = deque([node])
+    visited[node] = True
+    team = []
 
     while queue:
         node = queue.popleft()
-        next_node = graph[node]
+        team.append(node)
 
-        cycle.append(node)
-        
+        next_node = graph[node]
         if visited[next_node]:
-            if next_node in cycle: # cycle안에 포함되어있지 않으면 이전 bfs에서 방문처리된거임
-                return len(cycle[cycle.index(next_node):])
+            if next_node in team:
+                return len(team[team.index(next_node):])
+            return 0
         else:
             visited[next_node] = True
             queue.append(next_node)
-    
-    return 0
 
 T = int(input())
 for _ in range(T):
-    N = int(input())
+    n = int(input())
     graph = [0] + list(map(int, input().split()))
-    visited = [False] * (N+1)
-    cnt = N
-    for i in range(1, N+1):
+    visited = [False] * (n+1)
+
+    cnt = n
+    for i in range(1, n+1):
         if not visited[i]:
             cnt -= bfs(i)
-
     print(cnt)
+    
