89d30796f05c1da1747101352910a9c85d078de8
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-03 15:02:25 +0900

    [level 3] Title: 베스트앨범, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,19 +1,28 @@
 from collections import defaultdict
-
 def solution(genres, plays):
-    genres_count = defaultdict(int)
-    genres_lst = defaultdict(list)
-    
-    for i in range(len(genres)):
-        genres_count[genres[i]] += plays[i]
-        genres_lst[genres[i]].append((plays[i], i))
-    
-    sorted_genres_count = sorted(genres_count.items(), key=lambda x:x[1], reverse=True)
-    
+    genres_rank = defaultdict(int)
+    for g, p in zip(genres, plays):
+        genres_rank[g] += p
+        
     answer = []
-    for genre in sorted_genres_count:
-        sorted_genres = sorted(genres_lst[genre[0]], key=lambda x:(-x[0], x[1]))
-        for g in sorted_genres[:2]:
-            answer.append(g[1])
+    idx = 0
+    for g, p in zip(genres, plays):
+        answer.append((genres_rank[g], p, idx))
+        idx += 1
+    answer.sort(key=lambda x:(-x[0], -x[1], x[2]))
     
-    return answer
+    cnt = 1
+    prior = answer[0][0]
+    n_answer = [answer[0][2]]
+    for a in answer[1:]:
+        if a[0] == prior:
+            if cnt == 2:
+                continue
+            else:
+                cnt += 1
+                n_answer.append(a[2])
+        else:
+            cnt = 1
+            prior = a[0]
+            n_answer.append(a[2])
+    return n_answer
