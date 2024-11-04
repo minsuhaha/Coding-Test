456a3dbc18cbfb73e27e5a1d20af472c1fdeb194
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-04 13:10:54 +0900

    [level 2] Title: 주식가격, Time: 63.31 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,27 @@
+from collections import deque
 def solution(prices):
-    answer = []
-    for i in range(len(prices)):
-        time = 0
-        for j in range(i+1, len(prices)):
-            time += 1
-            if prices[i] > prices[j]:
-                break
-        answer.append(time)
+    stack = []
+    queue = deque()
+    for idx, price in enumerate(prices):
+        queue.append((idx, price))
+    
+    down = []
+    while queue:
+        idx, price = queue.popleft()
         
+        while stack and stack[-1][1] > price:
+            ans = stack.pop()
+            down.append((ans[0] ,idx-ans[0]))           
+        stack.append((idx, price))
+    
+    n = len(prices)
+    answer = [0] * n
+    for idx, price in stack:
+        answer[idx] = (n-1)-idx
+    
+    for idx, rank in down:
+        answer[idx] = rank
+    
     return answer
-                
+    
+    
