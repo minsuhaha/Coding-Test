08114d83158f6f4c8de9f0cc3c24303caa5b727d
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-05 12:50:12 +0900

    [level 2] Title: 가장 큰 수, Time: 60.35 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,7 @@
 def solution(numbers):
-    # 사전식 비교 (문자열 활용 -> 문자열에서는 앞의 원소만 비교함)
-    numbers = list(map(str, numbers)) 
+    numbers = list(map(str, numbers))
     numbers.sort(key=lambda x:x*3, reverse=True)
     
-    return '0' if all(num == '0' for num in numbers) else ''.join(numbers)
+    if numbers[0] == '0':
+        return '0'
+    return ''.join(numbers)
