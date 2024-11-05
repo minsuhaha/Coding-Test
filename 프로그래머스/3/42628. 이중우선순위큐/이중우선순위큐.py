d1289eeb3cb801d8df792c43c71d785bdb17fcf0
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-05 12:00:28 +0900

    [level 3] Title: 이중우선순위큐, Time: 281.40 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,20 +1,22 @@
 import heapq
 def solution(operations):
-    min_heap = []
     max_heap = []
+    min_heap = []
     for operation in operations:
-        cal, num = operation.split()
-        if cal == 'I':
+        oper, num = operation.split()
+        if oper == 'I':
             heapq.heappush(min_heap, int(num))
             heapq.heappush(max_heap, -int(num))
-        elif cal == 'D' and min_heap: 
-            if num == '-1':
-                n = heapq.heappop(min_heap)
-                max_heap.remove(-n)     
+        elif oper == 'D':
+            if len(min_heap) == 0:
+                continue
             elif num == '1':
-                n = heapq.heappop(max_heap)
-                min_heap.remove(-n)
+                q = heapq.heappop(max_heap)
+                min_heap.remove(-q)
+            elif num == '-1':
+                q = heapq.heappop(min_heap)
+                max_heap.remove(-q)
     if min_heap:
-        min_num, max_num = heapq.heappop(min_heap), -heapq.heappop(max_heap)
-        return max_num, min_num
-    return 0, 0
+        return -heapq.heappop(max_heap), heapq.heappop(min_heap)
+    return [0,0]
+    
