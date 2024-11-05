98c4c1062fbd8ec0461df42813bb575cd2a06e21
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-05 21:28:27 +0900

    [level 2] Title: H-Index, Time: 0.18 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,10 @@
 def solution(citations):
     citations.sort(reverse=True)
-    for i in range(len(citations)):
-        if i+1 > citations[i]:
-            return i
-    return len(citations)
+    
+    # [6, 5, 3, 1, 0]
+    for idx, citation in enumerate(citations):
+        if idx+1 > citation:
+            return idx
+    return idx+1
+    
+    
