e7011023a64752e2cab5811d5c836568d090742b
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-06 12:00:45 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.08 ms
+메모리: 10.2 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 09일 09:46:02
+2024년 11월 06일 12:00:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,10 @@
 def solution(brown, yellow):
     total = brown + yellow
-    for height in range(3, total):
-        if total % height == 0:
-            width = total // height
+    # col, row 최소 3
+    for row in range(3, total):
+        if total % row == 0:
+            col = total // row
+            if (col-2) * (row-2) == yellow:
+                return col, row
             
-            if (width-2)*(height-2) == yellow:
-                return width, height
             
-            
-            
-    
-    
