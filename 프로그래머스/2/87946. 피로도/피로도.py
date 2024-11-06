6e01509da7639420730b737160dfaceb00b3911a
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-06 12:15:04 +0900

    [level 2] Title: 피로도, Time: 44.51 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,18 @@
 def solution(k, dungeons):
+    
     def dfs(hp, cnt):
         nonlocal max_cnt
         max_cnt = max(max_cnt, cnt)
         
-        for i in range(len(dungeons)):
-            if not visited[i] and hp >= dungeons[i][0]:
-                visited[i] = True
-                dfs(hp-dungeons[i][1], cnt+1)
-                visited[i] = False
-    
+        for idx, dungeon in enumerate(dungeons):
+            if not visited[idx]:
+                if hp >= dungeon[0]:
+                    visited[idx] = True
+                    dfs(hp-dungeon[1], cnt+1)
+                    visited[idx] = False
+        
     
     max_cnt = 0
-    visited = [False] * len(dungeons)
+    visited = [False] * (len(dungeons))
     dfs(k, 0)
     return max_cnt
-    
-    
