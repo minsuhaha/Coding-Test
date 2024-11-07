38def2ea86d1d6d82028a0060376dc6b4c424cff
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-07 09:58:13 +0900

    [level 2] Title: 큰 수 만들기, Time: 111.14 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,9 +1,13 @@
 def solution(number, k):
     stack = []
-    for i in range(len(number)):
-        while stack and k > 0 and stack[-1] < number[i]:
-            stack.pop()
-            k -= 1
-        stack.append(number[i])
-    
-    return ''.join(map(str, stack[:len(stack)-k]))
+    for n in number:
+        while stack and k > 0:
+            if stack[-1] < n:
+                stack.pop()
+                k -= 1
+            else:
+                break
+        stack.append(n)
+    return ''.join(stack[:len(stack)-k])
+                
+        
