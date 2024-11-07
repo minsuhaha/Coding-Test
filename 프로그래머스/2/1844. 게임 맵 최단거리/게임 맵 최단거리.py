34935bcb52ca67ea834834eccd0e8a2cd9cec57b
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-07 14:25:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,27 @@
 from collections import deque
 def solution(maps):
-    
     n, m = len(maps), len(maps[0])
-    dx = [1, -1, 0, 0]
+    dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    visited = [[False]*m for _ in range(n)]
-    def bfs(x, y, z):
-        queue = deque([(x,y,z)])
-        visited[x][y] = True
+    
+    def bfs(x, y):
+        queue = deque([(x, y)])
+        visited[x][y] = 1
         
         while queue:
-            x, y, z = queue.popleft()
+            x, y = queue.popleft()
             
             if x == n-1 and y == m-1:
-                return z
+                return visited[x][y]
             
             for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+                nx, ny = x + dx[i], y + dy[i]
                 
                 if 0<=nx<n and 0<=ny<m:
-                    if maps[nx][ny] == 1 and not visited[nx][ny]:
-                        visited[nx][ny] = True
-                        queue.append((nx,ny,z+1))
+                    if not visited[nx][ny] and maps[nx][ny] == 1:
+                        visited[nx][ny] = visited[x][y] + 1
+                        queue.append((nx, ny))
         return -1
     
-    return bfs(0,0,1)
+    visited = [[0]*m for _ in range(n)]
+    return bfs(0, 0)
