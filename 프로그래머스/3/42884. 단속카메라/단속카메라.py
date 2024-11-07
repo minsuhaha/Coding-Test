95e39baef382a5800c10152ea81d68e21fe356f3
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-07 11:38:10 +0900

    [level 3] Title: 단속카메라, Time: 1.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,14 +1,10 @@
 def solution(routes):
     routes.sort(key=lambda x:x[1])
-    
     cnt = 1
-    spot = routes[0][1] # -15
+    exit = routes[0][1]
     for i in range(1, len(routes)):
-        if routes[i][0] > spot:
-            spot = routes[i][1]
+        if routes[i][0] > exit:
+            exit = routes[i][1]
             cnt += 1
-    
     return cnt
-            
         
-    
