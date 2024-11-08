dd6f1ab690e3204c09ddc837231d29c959f6d89d
Ha Min Su <105342203+minsuhaha@users.noreply.github.com>
2024-11-08 11:40:41 +0900

    [level 2] Title: 스킬트리, Time: 0.43 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,16 +1,30 @@
+import copy
 from collections import defaultdict
 def solution(skill, skill_trees):
+    skill_dict = {s:[] for s in skill}
+    indegree = {s:0 for s in skill}
+    
+    for i in range(len(skill)-1):
+        for j in range(i+1, len(skill)):
+            skill_dict[skill[i]].append(skill[j])
+            indegree[skill[j]] += 1
+    
     cnt = 0
     for skill_tree in skill_trees:
-        skill_order = []
-        for st in skill_tree:
-            if st in skill:
-                skill_order.append(st)
-        if ''.join(skill_order) == skill[:len(skill_order)]:
-            cnt += 1
-    return cnt
-        
-            
-        
+        indegree_copy = copy.deepcopy(indegree)
+        flag = True
+        for skill in skill_tree:
+            if skill not in skill_dict:
+                continue
+            if indegree_copy[skill] > 0:
+                flag = False
+                break
+            if indegree_copy[skill] == 0:
+                for s in skill_dict[skill]:
+                    indegree_copy[s] -= 1
     
+        if flag:
+            cnt += 1
+    return cnt   
+                
     
